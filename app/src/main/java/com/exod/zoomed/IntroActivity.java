f4fcfc87d3d76843890ffbccd24f2a597e8b8c12
= <=>
2019-05-30 03:24:09 +0100

    allen

--- app/src/main/java/com/exod/zoomed/IntroActivity.java
+++ app/src/main/java/com/exod/zoomed/IntroActivity.java
@@ -46,10 +46,10 @@ public class IntroActivity extends AppCompatActivity {
 
         if (restorePrefData()) {
 
-            //Intent mainActivity = new Intent(getApplicationContext(), MainActivity.class );
-            //startActivity(mainActivity);
-            Intent loginActivity = new Intent(getApplicationContext(), LoginActivity.class);
-            startActivity(loginActivity);
+            Intent mainActivity = new Intent(getApplicationContext(), MainActivity.class );
+            startActivity(mainActivity);
+            //Intent loginActivity = new Intent(getApplicationContext(), LoginActivity.class);
+            //startActivity(loginActivity);
             finish();
 
         }
@@ -151,11 +151,11 @@ public class IntroActivity extends AppCompatActivity {
 
                 //open main activity
 
-                //Intent mainActivity = new Intent(getApplicationContext(), MainActivity.class);
-                //startActivity(mainActivity);
+                Intent mainActivity = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(mainActivity);
 
-                Intent loginActivity = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(loginActivity);
+                //Intent loginActivity = new Intent(getApplicationContext(), LoginActivity.class);
+                //startActivity(loginActivity);
                 // also we need to save a boolean value to storage so next time when the user run the app
                 // we could know that he is already checked the intro screen activity
                 // i'm going to use shared preferences to that process
